5b4226ea88fd3907e3493503bbea9421982cd0b7
youngjo park <anyways5188@gmail.com>
2025-05-02 16:07:01 +0900

    [feature] 30p

--- CRA_TDD5_Baseball/baseball.cpp
+++ CRA_TDD5_Baseball/baseball.cpp
@@ -5,8 +5,14 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& string) {
-		if (strint.length() != 3) {
+		if (string.length() != 3) {
 			throw length_error("Must be three letters.");
+		}
+
+		for (char ch : string) {
+			if (ch < '0' || ch > '9') {
+				throw invalid_argument("Must be number");
+			}
 		}
 	}
 
--- CRA_TDD5_Baseball/baseball_test.cpp
+++ CRA_TDD5_Baseball/baseball_test.cpp
@@ -7,6 +7,12 @@ TEST(BaseballGame, ThrowExceptionWhenInpustLengthIsUnmatched)
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
 
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar)
+{
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
+
 TEST(BaseballTS, exceptionTC1)
 {
 
