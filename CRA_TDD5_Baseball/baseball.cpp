70026eb9d94ead1809d88210035c0f971e8d5385
youngjo park <anyways5188@gmail.com>
2025-05-02 16:27:49 +0900

    [refactor] 50p

--- CRA_TDD5_Baseball/baseball.cpp
+++ CRA_TDD5_Baseball/baseball.cpp
@@ -2,23 +2,47 @@
 
 using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
+	explicit Baseball(const string& question)
+		:question(question) {
+	}
+
+	bool isDuplicatedNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
+			if (ch >= '0' && ch <= '9') continue; {
 				throw invalid_argument("Must be number");
 			}
 		}
 
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]) {
+		if (isDuplicatedNumber(guessNumber)) {
 			throw invalid_argument("Must not have the same number");
 		}
 	}
+
+	GuessResult guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+		return { true, 3, 0 };
+	}
+
+private:
+	string question;
 };
