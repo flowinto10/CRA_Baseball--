233475235ef018b841c1325e8f0ca07f3815c6e7
youngjo park <anyways5188@gmail.com>
2025-05-02 16:16:28 +0900

    [feature] 39p

--- CRA_TDD5_Baseball/baseball_test.cpp
+++ CRA_TDD5_Baseball/baseball_test.cpp
@@ -20,6 +20,7 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase)
 {
 	assertIllegalArgument(string("12"));
 	assertIllegalArgument(string("12s"));
+	assertIllegalArgument(string("121"));
 }
 
 int main() {
